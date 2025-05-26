3a7768c51591d37b9d4c13102ad4d0f918153cc4
xiaokun <1240816911@qq.com>
2025-05-27 00:39:43 +0800

    修改配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    id("io.izzel.taboolib") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.22" apply false
+    id("io.izzel.taboolib") version "2.0.23" apply false
+    kotlin("jvm") version "2.0.21" apply false
 }
 
 subprojects {
@@ -24,13 +24,17 @@ subprojects {
         env {
             install(Basic, Bukkit, BukkitUtil)
             install(CommandHelper)
+            repoTabooLib = "https://maven.mcplugin.top/repository/minecraft/"
         }
-        version { taboolib = "6.2.0-beta33" }
+        version { taboolib = "6.2.3.2" }
     }
 
     // 仓库
     repositories {
         mavenCentral()
+        maven {
+            url = uri("https://maven.mcplugin.top/repository/minecraft/")
+        }
     }
     // 依赖
     dependencies {
